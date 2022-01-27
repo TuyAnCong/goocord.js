ae14ff0e992d354c80d5248df2abfe85dcf14fdd
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-01-27 16:21:51 +0700

    Create .homeDir.js

---
+++ client/.homeDir.js
@@ -0,0 +1,25 @@
+/* /data/data/GooCord.js/name.js homeDir:
+   ^    ^ 
+  ~~~~~~~~~~ */
+const { requires_home_dir } = require("./config.json")
+const { home_dir } = require("./config.json")
+const home_dir = /data/data
+const requires_home_dir = true
+/////////////////VARIABLE:
+/////home_dir - Set a Folder Home Dir
+/////requires_home_dir - Require a folder home dir
+/////////////////Thank you:
+/////discord.js, aoi.js
+/////////////////ASCII:
+/* ____                                               __                
+/\  _`\                                            /\ \               
+\ \ \L\_\    ___     ___     ___     ___    _ __   \_\ \              
+ \ \ \L_L   / __`\  / __`\  /'___\  / __`\ /\`'__\ /'_` \             
+  \ \ \/, \/\ \L\ \/\ \L\ \/\ \__/ /\ \L\ \\ \ \/ /\ \L\ \            
+   \ \____/\ \____/\ \____/\ \____\\ \____/ \ \_\ \ \___,_\           
+    \/___/  \/___/  \/___/  \/____/ \/___/   \/_/  \/__,_ /           
+                                                                      
+                                                                      @Goocord*/
+/*
+@author Goocord
+*/
