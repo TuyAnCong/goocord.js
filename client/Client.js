c15f8421ab68ffd4bb379dd5053503f1dc8eabc8
TuyAnCong <97296405+TuyAnCong@users.noreply.github.com>
2022-04-27 16:46:18 +0700

    Client()

--- client/Client.js
+++ client/Client.js
@@ -1,2 +1,9 @@
-const requires = require("goocord.js")
-${user.tag.bot} = requires.tagbot
+/* GooCord Database
+*
+*@param {String} request Request error
+*@param {String} media Allow you bot send media and embed
+*/
+function Client(Token) {
+ if(this.Token != "")
+ TypeError('Token Incorrect!, Go to Discord Developer Applications to Get Token')
+}
